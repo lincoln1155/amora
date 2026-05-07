33b05e79e2a5d1edd88492e01d4417209bcf8d2c
lincoln1155 <lincoln07oliveira@gmail.com>
2026-05-06 23:43:26 -0300

    fix: include frontend/src/lib/colors.js (was gitignored)

---
+++ frontend/src/lib/colors.js
@@ -0,0 +1,10 @@
+export const EVENT_COLORS = [
+  '#c4b5fd', // violet
+  '#fbcfe8', // pink
+  '#bfdbfe', // blue
+  '#bbf7d0', // green
+  '#fde68a', // amber
+  '#fed7aa', // orange
+]
+
+export const DEFAULT_EVENT_COLOR = EVENT_COLORS[0]
